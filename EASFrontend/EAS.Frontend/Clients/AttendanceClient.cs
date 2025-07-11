15ef75bbc62f63a444834190addf42c03da1fb34
Moiz Ahmed <moeezinbox@gmail.com>
2025-07-11 18:31:10 +0530

    New index page, dynamic row number & made table as responsive table.

--- EASFrontend/EAS.Frontend/Clients/AttendanceClient.cs
+++ EASFrontend/EAS.Frontend/Clients/AttendanceClient.cs
@@ -6,10 +6,19 @@ namespace EAS.Frontend.Clients;
 public class AttendanceClient(HttpClient httpClient)
 {
    public async Task<AttendanceSummary[]> GetAttendanceSummariesAsync()
-      => await httpClient.GetFromJsonAsync<AttendanceSummary[]>("attendance") ?? [];
+   {
+      var summaries = await httpClient.GetFromJsonAsync<AttendanceSummary[]>($"attendance/search?fromdate={DateTime.Now.ToString("yyyy-MM-dd")}&todate={DateTime.Now.ToString("yyyy-MM-dd")}") ?? [];
+      return summaries;
+   }
+
+   public async Task<AttendanceSummary[]> GetAttendanceSearchAsync()
+   {
+      var summaries = await httpClient.GetFromJsonAsync<AttendanceSummary[]>($"attendance/search?fromdate={DateTime.Now.ToString("yyyy-MM-dd")}&todate={DateTime.Now.ToString("yyyy-MM-dd")}") ?? [];
+      return summaries;
+   }
 
    public async Task UpdateAttendanceAsync(AttendanceSummary updateAttendance)
    {
-      await httpClient.PutAsJsonAsync($"attendance/{updateAttendance.Id}", updateAttendance);
+      await httpClient.PostAsJsonAsync($"attendance/edit", updateAttendance);
    }
 }
