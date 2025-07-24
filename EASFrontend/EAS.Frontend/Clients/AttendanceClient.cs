ad004e2d061a1c732fe6556eab152449ad02e42b
Moiz Ahmed <moeezinbox@gmail.com>
2025-07-24 14:36:41 +0530

    Send DSR Reminder by email-id and Total Attendance in Today's Attendance page.

--- EASFrontend/EAS.Frontend/Clients/AttendanceClient.cs
+++ EASFrontend/EAS.Frontend/Clients/AttendanceClient.cs
@@ -1,16 +1,17 @@
-using System;
 using EAS.Frontend.Models;
 
 namespace EAS.Frontend.Clients;
 
 public class AttendanceClient(HttpClient httpClient)
 {
+   // Fetch attendance summaries for today by default
    public async Task<AttendanceSummary[]> GetAttendanceSummariesAsync()
    {
       var summaries = await httpClient.GetFromJsonAsync<AttendanceSummary[]>($"attendance/search?fromdate={DateTime.Now.ToString("yyyy-MM-dd")}&todate={DateTime.Now.ToString("yyyy-MM-dd")}") ?? [];
       return summaries;
    }
 
+   // Fetch attendance summaries for a specific date range
    public async Task<AttendanceSummary[]> GetAttendanceSearchAsync(AttendanceFilters attendanceFilters)
    {
       string parameters = string.Empty;
@@ -40,17 +41,20 @@ public class AttendanceClient(HttpClient httpClient)
       return summaries;
    }
 
+   // Update attendance for a specific user
    public async Task UpdateAttendanceAsync(AttendanceSummary updateAttendance)
    {
       await httpClient.PostAsJsonAsync($"attendance/edit", updateAttendance);
    }
 
+   // Fetch attendance summaries for yesterday
    public async Task<AttendanceSummary[]> GetYesterdayAttendanceSummariesAsync()
    {
       var summaries = await httpClient.GetFromJsonAsync<AttendanceSummary[]>($"attendance/search?fromdate={DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd")}&todate={DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd")}") ?? [];
       return summaries;
    }
 
+   // Fetch attendance summaries for DSR reminders
    public async Task<AttendanceSummary[]> GetAttendanceSearchForDSRReminderAsync(AttendanceFilters attendanceFilters)
    {
       string parameters = string.Empty;
@@ -80,8 +84,12 @@ public class AttendanceClient(HttpClient httpClient)
       return summaries;
    }
 
+   // Send DSR reminder for a specific user
    public async Task SendDSRReminderAsync(AttendanceSummary updateAttendance)
    {
-      await httpClient.PostAsJsonAsync($"attendance/send-dsr-reminder", updateAttendance);
+      var response = await httpClient.GetAsync($"attendance/send-dsr-reminder?email={updateAttendance.EmailId}");
+
+      var content = await response.Content.ReadAsStringAsync();
+      Console.WriteLine($"[Error] Response Body: {content}");
    }
 }
